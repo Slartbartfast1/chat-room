585f8f3320c225b097a5d620e387fe8b43557e50
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-14 02:54:32 +0800

    主页

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 var app = require('express')();
+var bodyParser = require('body-parser')
+var session=require('express-session')
+var router = require('./router');
+
+//配置post插件
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+//session
+app.use(session({
+    secret: 'oh my gosh',
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(router);
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-app.get('/signup', function(req, res){
-    res.sendFile(__dirname + '/signup.html');
-});
-app.get('/signin', function(req, res){
-    res.sendFile(__dirname + '/signin.html');
-});
-
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 });
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
     });
 });
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     socket.broadcast.emit('hi');
 });
 
 
-
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
