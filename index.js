a925355705a24af93048ad037cda3b56783ae5cf
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-14 16:48:24 +0800

    入口

--- index.js
+++ index.js
@@ -13,25 +13,62 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }))
-
+app.engine('art', require('express-art-template'));
+app.set('view options', {
+    debug: process.env.NODE_ENV !== 'production'
+});
 app.use(router);
+var roomInfo = {}
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
+
+io.on('connection',function(socket){
+    var url = socket.request.headers.referer;
+    var splited = url.split('/');
+    var roomID = splited[splited.length - 1];// 获取房间ID
+
+
+    socket.on('join', function (nickName) {
+        user = nickName;
+        // 将用户昵称加入房间名单中
+        if (!roomInfo[roomID]) {
+            roomInfo[roomID] = [];
+        }
+        if(roomInfo[roomID].indexOf(user)===-1){
+            roomInfo[roomID].push(user);
+        }
+
+
+        socket.join(roomID);    // 加入房间
+        // 通知房间内人员
+        io.to(roomID).emit('sys', user + '加入了房间', roomInfo[roomID]);
+        console.log(user + '加入了' + roomID,roomInfo[roomID]);
     });
-});
-io.on('connection', function (socket) {
     socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
+        var room = Object.keys(socket.rooms)[1]; //这是当前socket的房间
+        io.to(room).emit('chat message', msg);
+    })
+
+    //失去连接
+    socket.on('leave', function () {
+        socket.emit('disconnect');
     });
-});
-io.on('connection', function (socket) {
-    socket.broadcast.emit('hi');
-});
+
+    socket.on('disconnect', function () {
+        // 从房间名单中移除
+        var index = roomInfo[roomID].indexOf(user);
+        if (index !== -1) {
+            roomInfo[roomID].splice(index, 1);
+        }
+
+        socket.leave(roomID);    // 退出房间
+        io.to(roomID).emit('sys', user + '退出了房间', roomInfo[roomID]);
+        console.log(user + '退出了' + roomID);
+    });
+})
+
+
 
 
 http.listen(3000, function () {
